f92b9ca08c0160d4ffd5a340d245884472190737
Riley Snyder <riley.snyder@harness.io>
2022-09-22 15:51:39 -0500

    imported existing resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,26 @@
+resource "aws_s3_bucket" "riley-snyder-harness-io" {
+  bucket = "riley-snyder-harness-io"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "riley-snyder-harness-io" {
+  bucket = aws_s3_bucket.riley-snyder-harness-io.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "riley-snyder-harness-io" {
+  bucket = aws_s3_bucket.riley-snyder-harness-io.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 module "ccm" {
   source              = "github.com/rssnyder/terraform-aws-harness-ccm.git"
   external_id         = "harness:891928451355:wlgELJ0TTre5aZhzpt8gVA"
