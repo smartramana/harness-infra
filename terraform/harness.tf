e0a068610159dae74abaf84aa7f97b4522ce1916
Riley Snyder <riley.snyder@harness.io>
2022-10-13 12:21:05 -0500

    add account connectors

--- terraform/harness.tf
+++ terraform/harness.tf
@@ -16,7 +16,7 @@ resource "harness_platform_usergroup" "approvers" {
   project_id         = harness_platform_project.development.id
   externally_managed = false
   users = [
-    "riley.snyder@harness.io"
+    "4JCQs46YTxKawHSWVW6nLA"
   ]
   notification_configs {
     type        = "EMAIL"
@@ -43,4 +43,39 @@ resource "harness_platform_connector_github" "Github" {
       token_ref = "account.gh_pat"
     }
   }
+}
+
+resource "harness_platform_connector_docker" "dockerhub" {
+  identifier = "dockerhub"
+  name       = "dockerhub"
+  type       = "DockerHub"
+  url        = "https://registry.hub.docker.com/v2/"
+
+  credentials {
+    username     = "rileysnyderharnessio"
+    password_ref = "account.dockerhub"
+  }
+}
+
+resource "harness_platform_connector_artifactory" "harnessartifactory" {
+  identifier = "harnessartifactory"
+  name       = "harness-artifactory"
+  url        = "https://harness.jfrog.io/artifactory/"
+}
+
+resource "harness_platform_connector_aws" "sales" {
+  identifier = "sales"
+  name       = "sales"
+
+  manual {
+    access_key     = "AKIA3B4U6ZRO63H37QA3"
+    secret_key_ref = "account.salesadminkey"
+  }
+}
+
+resource "harness_platform_connector_prometheus" "saprom" {
+  identifier         = "saprom"
+  name               = "sa-prom"
+  url                = "http://prometheus-kube-prometheus-prometheus.prometheus.svc.cluster.local:9090/"
+  delegate_selectors = ["riley-sa-gcp"]
 }
