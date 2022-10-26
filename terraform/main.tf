70d93ed938a5b9ed4a05172d23fd4388399d5164
Riley Snyder <riley.snyder@harness.io>
2022-10-26 11:19:04 -0500

    rssnyder@github

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_public_access_block" "riley-snyder-harness-io" {
 }
 
 module "ccm" {
-  source                  = "git::https://github.com/rssnyder/test-private-tf-module.git?ref=main" 
+  source                  = "git::https://rssnyder@github.com/rssnyder/test-private-tf-module.git?ref=main" 
   external_id             = "harness:891928451355:wlgELJ0TTre5aZhzpt8gVA"
   additional_external_ids = ["harness:891928451355:V2iSB2gRR_SxBs0Ov5vqCQ"]
   enable_events           = true
