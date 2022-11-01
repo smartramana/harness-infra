a874e7a95c7c5c6cd91324602a638de4adbdc122
Riley Snyder <riley.snyder@harness.io>
2022-11-01 15:36:33 -0500

    us-west-2

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ module "vpc" {
   name = "sa-lab"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${local.region}a", "${local.region}b"]
+  azs             = ["us-west-2a", "us-west-2b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24"]
 
