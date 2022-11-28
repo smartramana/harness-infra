4cf993ea478fc6320a5d1ce248a4c60331c46878
Riley Snyder <riley.snyder@harness.io>
2022-11-28 14:12:13 -0600

    delete iam users

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,31 +1,3 @@
-data "aws_iam_group" "administrators" {
-  group_name = "Administrators"
-}
-
-resource "aws_iam_user" "rileysnyderharnessio" {
-  name = "riley.snyder@harness.io"
-}
-
-resource "aws_iam_user_group_membership" "rileysnyderharnessio-administrators" {
-  user = aws_iam_user.rileysnyderharnessio.name
-
-  groups = [
-    data.aws_iam_group.administrators.group_name
-  ]
-}
-
-resource "aws_iam_user" "rileysnyderharnessio-connector" {
-  name = "riley.snyder@harness.io-connector"
-}
-
-resource "aws_iam_user_group_membership" "rileysnyderharnessio-connector-administrators" {
-  user = aws_iam_user.rileysnyderharnessio-connector.name
-
-  groups = [
-    data.aws_iam_group.administrators.group_name
-  ]
-}
-
 data "aws_iam_policy_document" "rileysnyderharnessio-assumed" {
   statement {
     actions = ["sts:AssumeRole"]
