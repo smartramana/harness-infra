a231d897943ec36ff739b4d8343bc634c324d132
Riley Snyder <riley.snyder@harness.io>
2022-11-29 09:16:53 -0600

    add ecs scheduler

--- terraform/delegate.tf
+++ terraform/delegate.tf
@@ -63,3 +63,175 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 #   subnets    = module.vpc.private_subnets
 #   cluster_id = module.delegate.aws_ecs_cluster
 # }
+
+# Schedle task to only run durring work hours
+
+data "aws_iam_policy_document" "ecs_scheduler_lambda" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "lambda.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_scheduler_lambda" {
+  name               = "ecs_scheduler_lambda"
+  assume_role_policy = data.aws_iam_policy_document.ecs_scheduler_lambda.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_scheduler_lambda" {
+  role       = aws_iam_role.ecs_scheduler_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_scheduler_lambda_logs" {
+  role       = aws_iam_role.ecs_scheduler_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "archive_file" "ecs_scheduler" {
+  type        = "zip"
+  output_path = "/tmp/ecs_scheduler.zip"
+  source {
+    content  = <<EOF
+from os import getenv
+
+import boto3
+
+
+def lambda_handler(event, context):
+    """
+    on an event, start or stop an ecs task
+    """
+
+    cluster = getenv("CLUSTER")
+    service = getenv("SERVICE")
+
+    action = event.get("action", "stop")
+
+    client = boto3.client("ecs")
+
+    if action == "start":
+        print(f"starting {cluster}/{service}")
+        client.update_service(
+            cluster=cluster,
+            service=service,
+            desiredCount=1,
+        )
+    else:
+        print(f"stopping {cluster}/{service}")
+        client.update_service(
+            cluster=cluster,
+            service=service,
+            desiredCount=0,
+        )
+EOF
+    filename = "main.py"
+  }
+}
+
+resource "aws_lambda_function" "ecs_scheduler" {
+
+  filename         = data.archive_file.ecs_scheduler.output_path
+  source_code_hash = data.archive_file.ecs_scheduler.output_base64sha256
+
+  function_name = "ecs_scheduler"
+  role          = aws_iam_role.ecs_scheduler_lambda.arn
+  handler       = "main.lambda_handler"
+
+  runtime = "python3.9"
+
+  environment {
+    variables = {
+      CLUSTER = module.delegate.aws_ecs_cluster,
+      SERVICE = module.delegate.aws_ecs_service
+    }
+  }
+}
+
+data "aws_iam_policy_document" "ecs_scheduler" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "scheduler.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_scheduler" {
+  name               = "ecs_scheduler"
+  assume_role_policy = data.aws_iam_policy_document.ecs_scheduler.json
+}
+
+resource "aws_iam_policy" "ecs_scheduler" {
+  name        = "ecs_scheduler"
+  description = "Policy for triggering lambda"
+
+  policy = <<EOF
+{
+   "Version": "2012-10-17",
+   "Statement": [
+       {
+           "Sid": "Invoke",
+           "Effect": "Allow",
+           "Action": [
+               "lambda:InvokeFunction"
+           ],
+           "Resource": [
+              "${aws_lambda_function.ecs_scheduler.arn}"
+           ]
+       }
+   ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_scheduler" {
+  role       = aws_iam_role.ecs_scheduler.name
+  policy_arn = aws_iam_policy.ecs_scheduler.arn
+}
+
+resource "aws_scheduler_schedule" "ecs_scheduler_start" {
+  name = "ecs_scheduler_start"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "cron(0 13 ? * MON-FRI *)"
+
+  target {
+    arn      = aws_lambda_function.ecs_scheduler.arn
+    role_arn = aws_iam_role.ecs_scheduler.arn
+    input = jsonencode({
+      action = "start"
+    })
+  }
+}
+
+resource "aws_scheduler_schedule" "ecs_scheduler_stop" {
+  name = "ecs_scheduler_stop"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "cron(0 21 ? * MON-FRI *)"
+
+  target {
+    arn      = aws_lambda_function.ecs_scheduler.arn
+    role_arn = aws_iam_role.ecs_scheduler.arn
+    input = jsonencode({
+      action = "stop"
+    })
+  }
+}
