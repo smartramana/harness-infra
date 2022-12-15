f7cbffa2ac99efb756c7a9954e6fd41ed056177b
Riley Snyder <riley.snyder@harness.io>
2022-12-15 10:29:08 -0600

    bump ccm ci pool 0 default

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,11 +22,14 @@ resource "aws_s3_bucket_public_access_block" "riley-snyder-harness-io" {
 }
 
 module "ccm" {
-  source                  = "git::https://github.com/harness-community/terraform-aws-harness-ccm.git?ref=0.0.1"
+  source = "git::https://github.com/harness-community/terraform-aws-harness-ccm.git?ref=0.0.2"
+  # source                  = "../../terraform-aws-harness-ccm"
   external_id             = "harness:891928451355:wlgELJ0TTre5aZhzpt8gVA"
   additional_external_ids = ["harness:891928451355:V2iSB2gRR_SxBs0Ov5vqCQ"]
   enable_events           = true
   enable_optimization     = true
+  enable_governance       = true
+  governance_policy_arn   = aws_iam_policy.delegate_aws_access.arn
   prefix                  = "riley-"
 }
 
