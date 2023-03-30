ad0f7d8c6d9fc59f1d1ca4699c95a2c1a41d863e
Riley Snyder <riley.snyder@harness.io>
2023-03-30 09:42:08 -0500

    test

--- terraform/harness_orgs.tf
+++ terraform/harness_orgs.tf
@@ -6,6 +6,14 @@ module "solution-architecture" {
   name = "solution-architecture"
 }
 
+module "test" {
+  source  = "harness-community/structure/harness//modules/organizations"
+  version = "0.1.2"
+  # source = "../../terraform-harness-structure/modules/organizations"
+
+  name = "test"
+}
+
 data "harness_platform_organization" "default" {
   identifier = "default"
   name       = "default"
