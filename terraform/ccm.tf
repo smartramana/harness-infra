0535d999b65d7fc0b1a1c1ecb6102f697e52e418
Riley Snyder <riley.snyder@harness.io>
2023-08-03 16:14:41 -0500

    update ccm module

--- terraform/ccm.tf
+++ terraform/ccm.tf
@@ -1,3 +1,30 @@
+module "ccm" {
+  source  = "harness-community/harness-ccm/aws"
+  version = "0.1.0-beta.1"
+
+  # source = "../../terraform-aws-harness-ccm"
+
+  s3_bucket_arn = "arn:aws:s3:::riley-harness-ccm"
+  external_id   = "harness:891928451355:wlgELJ0TTre5aZhzpt8gVA"
+  additional_external_ids = [
+    "harness:891928451355:V2iSB2gRR_SxBs0Ov5vqCQ"
+  ]
+  enable_billing          = true
+  enable_events           = true
+  enable_optimization     = true
+  enable_governance       = true
+  enable_commitment_read  = true
+  enable_commitment_write = true
+  governance_policy_arns = [
+    aws_iam_policy.delegate_aws_access.arn
+  ]
+  prefix = "riley-"
+  secrets = [
+    "arn:aws:secretsmanager:us-west-2:759984737373:secret:sa/ca-key.pem-HYlaV4",
+    "arn:aws:secretsmanager:us-west-2:759984737373:secret:sa/ca-cert.pem-kq8HQl"
+  ]
+}
+
 resource "harness_platform_connector_azure_cloud_cost" "azure-sales-ccm" {
   identifier = "azuresalesccm"
   name       = "azure-sales-ccm"
--- terraform/main.tf
+++ terraform/main.tf
@@ -21,23 +21,6 @@ resource "aws_s3_bucket_public_access_block" "riley-snyder-harness-io" {
   restrict_public_buckets = true
 }
 
-# module "ccm" {
-#   source  = "harness-community/harness-ccm/aws"
-#   version = "0.0.3"
-#   # source                  = "../../terraform-aws-harness-ccm"
-#   external_id             = "harness:891928451355:wlgELJ0TTre5aZhzpt8gVA"
-#   additional_external_ids = ["harness:891928451355:V2iSB2gRR_SxBs0Ov5vqCQ"]
-#   enable_events           = true
-#   enable_optimization     = true
-#   enable_governance       = true
-#   # governance_policy_arn   = aws_iam_policy.delegate_aws_access.arn
-#   prefix = "riley-"
-#   secrets = [
-#     "arn:aws:secretsmanager:us-west-2:759984737373:secret:sa/ca-key.pem-HYlaV4",
-#     "arn:aws:secretsmanager:us-west-2:759984737373:secret:sa/ca-cert.pem-kq8HQl"
-#   ]
-# }
-
 resource "aws_ecr_repository" "rileysnyder" {
   name = "rileysnyder"
 }
