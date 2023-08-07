a5f2be6826d912f89d72817f4bec8edb8f1975d2
Riley Snyder <riley.snyder@harness.io>
2023-08-07 16:18:38 -0500

    module.ccm.external_id

--- terraform/ccm.tf
+++ terraform/ccm.tf
@@ -1,6 +1,6 @@
 module "ccm" {
   source  = "harness-community/harness-ccm/aws"
-  version = "0.1.0-beta.1"
+  version = "0.1.1-beta.1"
 
   # source = "../../terraform-aws-harness-ccm"
 
@@ -23,6 +23,24 @@ module "ccm" {
     "arn:aws:secretsmanager:us-west-2:759984737373:secret:sa/ca-key.pem-HYlaV4",
     "arn:aws:secretsmanager:us-west-2:759984737373:secret:sa/ca-cert.pem-kq8HQl"
   ]
+}
+
+resource "harness_platform_connector_awscc" "rileyharnessccm" {
+  identifier = "rileyharnessccm"
+  name       = "riley-harness-ccm"
+
+  account_id  = "759984737373"
+  report_name = "riley-harness-ccm"
+  s3_bucket   = "riley-harness-ccm"
+  features_enabled = [
+    "OPTIMIZATION",
+    "VISIBILITY",
+    "BILLING",
+  ]
+  cross_account_access {
+    role_arn    = module.ccm.cross_account_role
+    external_id = module.ccm.external_id
+  }
 }
 
 resource "harness_platform_connector_azure_cloud_cost" "azure-sales-ccm" {
@@ -48,24 +66,6 @@ resource "harness_platform_connector_azure_cloud_cost" "azure-sales-ccm-broken" 
   features_enabled = ["VISIBILITY", "OPTIMIZATION"]
   tenant_id        = "b229b2bb-5f33-4d22-bce0-730f6474e906"
   subscription_id  = "e8389fc5-0cb8-44ab-947b-c6cf62552be1"
-}
-
-resource "harness_platform_connector_awscc" "rileyharnessccm" {
-  identifier = "rileyharnessccm"
-  name       = "riley-harness-ccm"
-
-  account_id  = "759984737373"
-  report_name = "riley-harness-ccm"
-  s3_bucket   = "riley-harness-ccm"
-  features_enabled = [
-    "OPTIMIZATION",
-    "VISIBILITY",
-    "BILLING",
-  ]
-  cross_account_access {
-    role_arn    = "arn:aws:iam::759984737373:role/riley-HarnessCERole"
-    external_id = "harness:891928451355:wlgELJ0TTre5aZhzpt8gVA"
-  }
 }
 
 # resource "harness_platform_connector_awscc" "rileyharnessccmbroken" {
