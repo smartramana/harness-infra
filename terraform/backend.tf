36d005db13d92e32e65f6748c2bcf9635a5a618e
Riley Snyder <riley.snyder@harness.io>
2023-08-11 09:52:46 -0500

    test removing backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "s3" {
-    bucket = "riley-snyder-harness-io"
-    key    = "terraform/harness-infra"
-    region = "us-west-2"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "riley-snyder-harness-io"
+#     key    = "terraform/harness-infra"
+#     region = "us-west-2"
+#   }
+# }
 # terraform {
 #   backend "http" {
 #     address  = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/development/workspaces/harnessinfra/terraform-backend?accountIdentifier=wlgELJ0TTre5aZhzpt8gVA"
