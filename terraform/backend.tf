9608ba5f4131e95975465563fad4a7f2141c31d4
Riley Snyder <riley.snyder@harness.io>
2023-08-11 14:34:22 -0500

    nginx 3 (#15)
    * nginx 3
    
    * 4
    
    * 5

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,13 +5,3 @@
 #     region = "us-west-2"
 #   }
 # }
-# terraform {
-#   backend "http" {
-#     address  = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/development/workspaces/harnessinfra/terraform-backend?accountIdentifier=wlgELJ0TTre5aZhzpt8gVA"
-#     username = "harness"
-#     lock_address   = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/development/workspaces/harnessinfra/terraform-backend/lock?accountIdentifier=wlgELJ0TTre5aZhzpt8gVA"
-#     lock_method    = "POST"
-#     unlock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/development/workspaces/harnessinfra/terraform-backend/lock?accountIdentifier=wlgELJ0TTre5aZhzpt8gVA"
-#     unlock_method  = "DELETE"
-#   }
-# }
--- terraform/nginx.tf
+++ terraform/nginx.tf
@@ -1,54 +1,54 @@
-# resource "harness_platform_service" "nginx" {
-#   count      = 40
-#   identifier = "nginx_${count.index}"
-#   name       = "nginx_${count.index}"
-#   org_id     = data.harness_platform_organization.default.id
-#   project_id = harness_platform_project.development.id
-#   yaml       = <<EOF
-# service:
-#   name: nginx_${count.index}
-#   identifier: nginx_${count.index}
-#   tags: {}
-#   serviceDefinition:
-#     type: Kubernetes
-#     spec:
-#       manifests:
-#         - manifest:
-#             identifier: template
-#             type: K8sManifest
-#             spec:
-#               store:
-#                 type: Github
-#                 spec:
-#                   connectorRef: account.Github
-#                   gitFetchType: Branch
-#                   paths:
-#                     - deployment.yaml
-#                   repoName: rssnyder/template
-#                   branch: main
-#               valuesPaths:
-#                 - values.yaml
-#               skipResourceVersioning: false
-#       artifacts:
-#         primary:
-#           primaryArtifactRef: nginx
-#           sources:
-#             - spec:
-#                 connectorRef: account.dockerhub
-#                 imagePath: library/nginx
-#                 tag: <+input>
-#               identifier: nginx
-#               type: DockerRegistry
-#       variables:
-#         - name: port
-#           type: String
-#           description: ""
-#           value: <+input>
-#           default: "80"
-#         - name: replicas
-#           type: String
-#           description: ""
-#           value: <+input>
-#           default: "1"
-# EOF
-# }
+resource "harness_platform_service" "nginx" {
+  count      = 5
+  identifier = "nginx_${count.index}"
+  name       = "nginx_${count.index}"
+  org_id     = data.harness_platform_organization.default.id
+  project_id = harness_platform_project.development.id
+  yaml       = <<EOF
+service:
+  name: nginx_${count.index}
+  identifier: nginx_${count.index}
+  tags: {}
+  serviceDefinition:
+    type: Kubernetes
+    spec:
+      manifests:
+        - manifest:
+            identifier: template
+            type: K8sManifest
+            spec:
+              store:
+                type: Github
+                spec:
+                  connectorRef: account.Github
+                  gitFetchType: Branch
+                  paths:
+                    - deployment.yaml
+                  repoName: rssnyder/template
+                  branch: main
+              valuesPaths:
+                - values.yaml
+              skipResourceVersioning: false
+      artifacts:
+        primary:
+          primaryArtifactRef: nginx
+          sources:
+            - spec:
+                connectorRef: account.dockerhub
+                imagePath: library/nginx
+                tag: <+input>
+              identifier: nginx
+              type: DockerRegistry
+      variables:
+        - name: port
+          type: String
+          description: ""
+          value: <+input>
+          default: "80"
+        - name: replicas
+          type: String
+          description: ""
+          value: <+input>
+          default: "1"
+EOF
+}
