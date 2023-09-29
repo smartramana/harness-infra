75f4791c442d8307283a5079413b47e3282b78cb
Riley Snyder <riley.snyder@harness.io>
2023-09-29 14:09:40 -0500

    create ff service

--- terraform/harness_home_lab.tf
+++ terraform/harness_home_lab.tf
@@ -101,3 +101,60 @@ service:
 
 EOF
 }
+
+resource "harness_platform_service" "ff_relay_proxy" {
+  identifier = "ff_relay_proxy"
+  name       = "ff relay proxy"
+  org_id     = data.harness_platform_organization.default.id
+  project_id = harness_platform_project.home_lab.id
+  yaml       = <<EOF
+service:
+  name: ff relay proxy
+  identifier: ff_relay_proxy
+  orgIdentifier: default
+  projectIdentifier: home_lab
+  serviceDefinition:
+    spec:
+      manifests:
+        - manifest:
+            identifier: main
+            type: K8sManifest
+            spec:
+              store:
+                type: Github
+                spec:
+                  connectorRef: account.${harness_platform_connector_github.Github.id}
+                  gitFetchType: Branch
+                  paths:
+                    - ff-proxy.yaml
+                  repoName: rssnyder/test
+                  branch: master
+              valuesPaths:
+                - ff-proxy-values.yaml
+              skipResourceVersioning: false
+              enableDeclarativeRollback: false
+      artifacts:
+        primary:
+          primaryArtifactRef: <+input>
+          sources:
+            - spec:
+                connectorRef: account.${harness_platform_connector_docker.dockerhub.id}
+                imagePath: harness/ff-proxy
+                tag: <+input>
+                digest: ""
+              identifier: main
+              type: DockerRegistry
+      variables:
+        - name: port
+          type: String
+          description: ""
+          required: true
+          value: "7000"
+        - name: replicas
+          type: String
+          description: ""
+          required: true
+          value: "2"
+    type: Kubernetes
+EOF
+}
