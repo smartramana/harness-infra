58032ab16c6fa97f754b62b93ada3a1e70e9b901
Riley Snyder <riley.snyder@harness.io>
2023-09-29 13:06:33 -0500

    adding home_lab project

---
+++ terraform/harness_home_lab.tf
@@ -0,0 +1,84 @@
+resource "harness_platform_project" "home_lab" {
+  identifier = "home_lab"
+  name       = "home_lab"
+  org_id     = data.harness_platform_organization.default.id
+}
+
+resource "harness_platform_service" "harness_ccm_k8s_auto" {
+  identifier = "harness_ccm_k8s_auto"
+  name       = "harness_ccm_k8s_auto"
+  org_id     = data.harness_platform_organization.default.id
+  project_id = harness_platform_project.home_lab.id
+  yaml       = <<EOF
+service:
+  name: harness_ccm_k8s_auto
+  identifier: harness_ccm_k8s_auto
+  serviceDefinition:
+    type: Kubernetes
+    spec:
+      manifests:
+        - manifest:
+            identifier: main
+            type: K8sManifest
+            spec:
+              store:
+                type: Github
+                spec:
+                  connectorRef: account.${harness_platform_connector_github.Github.id}
+                  gitFetchType: Branch
+                  paths:
+                    - deployment.yaml
+                  repoName: harness-community/harness-ccm-k8s-auto
+                  branch: main
+              valuesPaths:
+                - values.yaml
+              skipResourceVersioning: false
+              enableDeclarativeRollback: false
+      artifacts:
+        primary:
+          primaryArtifactRef: <+input>
+          sources:
+            - spec:
+                connectorRef: account.${harness_platform_connector_docker.dockerhub.id}
+                imagePath: harnesscommunity/harness-ccm-k8s-auto
+                tag: <+input>
+                digest: ""
+              identifier: main
+              type: DockerRegistry
+  gitOpsEnabled: false
+EOF
+}
+
+resource "harness_platform_service" "longhorn" {
+  identifier = "longhorn"
+  name       = "longhorn"
+  org_id     = data.harness_platform_organization.default.id
+  project_id = harness_platform_project.home_lab.id
+  yaml       = <<EOF
+service:
+  name: longhorn
+  identifier: longhorn
+  orgIdentifier: default
+  projectIdentifier: Default_Project_1662659562703
+  serviceDefinition:
+    spec:
+      manifests:
+        - manifest:
+            identifier: main
+            type: K8sManifest
+            spec:
+              store:
+                type: Github
+                spec:
+                  connectorRef: account.${harness_platform_connector_github.Github.id}
+                  gitFetchType: Branch
+                  paths:
+                    - deploy/longhorn.yaml
+                  repoName: longhorn/longhorn
+                  branch: v1.5.x
+              skipResourceVersioning: false
+              enableDeclarativeRollback: false
+    type: Kubernetes
+
+EOF
+}
